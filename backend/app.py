525ba56b3a296d6feff5182cca7e4dcfbc488db9
Radu Lupan <github@lupan.ca>
2025-04-23 18:43:52 -0400

    Modified the S3 actions to use credentials from environment variables when running in Docker

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,6 @@
-import logging
+import os
 import boto3
 from flask import Flask, request, jsonify
-
-logging.basicConfig(level=logging.DEBUG)
-
-s3_client = boto3.client('s3')
 
 app = Flask(__name__)
 
@@ -19,11 +15,17 @@ def upload_file():
 
     file = request.files['file']
     try:
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+            region_name=os.getenv('AWS_DEFAULT_REGION')
+        )
         s3_client.upload_fileobj(file, "my-wetransfer-clone-bucket-0319cf63dee7", file.filename)
         return jsonify({'message': 'File successfully uploaded'}), 200
     except Exception as e:
-        logging.exception("Exception during S3 upload")
-        return jsonify({'error': 'File upload failed', 'detail': str(e)}), 500
+        print(f"Upload failed: {e}")
+        return jsonify({'error': 'File upload failed', 'message': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
